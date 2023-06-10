8404b8c21818086ede037fad63dd53cfa6a63dad
RoKivals <slava111103@gmail.com>
2023-06-11 00:52:52 +0300

    EDIT mistakes

--- Lab#2/main.cpp
+++ Lab#2/main.cpp
@@ -1,22 +1,22 @@
 #include "header.h"
 
 int main() {
-    // auto автоматически определяет типпеременной
-    auto dots = create_matrix();
-    int radius = 0;
-    std::cout << "Input radius of circle: ";
-    std::cin >> radius;
-    int error_ind = -1;
-    for (int i(0); i < dots.size(); ++i) {
-        if (!is_inside_circle(dots[i], radius)) {
-            error_ind = i;
-            number_of_inappropriate_dots(error_ind, dots, radius); 
-            break;
-        }
-    }
-    if (error_ind == -1) {
-        std::cout << "Average value of abscissa is " << avg_abscissa << '\n';
-        std::cout << "Average value of ordinate is " << avg_abscissa << '\n';
-    }
-    return 0;
+  // auto автоматически определяет типпеременной
+  auto dots = create_matrix();
+  int radius = 0;
+  std::cout << "Input radius of circle: ";
+  std::cin >> radius;
+  int error_ind = -1;
+  for (int i(0); i < dots.size(); ++i) {
+	if (!is_inside_circle(dots[i], radius)) {
+	  error_ind = i;
+	  number_of_inappropriate_dots(error_ind, dots, radius);
+	  break;
+	}
+  }
+  if (error_ind == -1) {
+	std::cout << "Average value of abscissa is " << avg_abscissa(dots) << '\n';
+	std::cout << "Average value of ordinate is " << avg_ordinate(dots) << '\n';
+  }
+  return 0;
 }
